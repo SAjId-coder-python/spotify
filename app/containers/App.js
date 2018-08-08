b640d9a8ccc5e594f77eb9040c87d374691e34e0
Bruno Carvalho <bruno@brunocarvalho.me>
2018-08-08 15:19:19 -0300

    Initial commit

---
+++ app/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import '../assets/scss/style.scss';
+
+export default props => (
+  <div className="container">
+    <h1>Shazam carai</h1>
+    <p>Nois que avoa</p>
+  </div>
+)
